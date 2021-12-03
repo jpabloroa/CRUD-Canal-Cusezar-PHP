0356e776372ef616739ce854fea2534da7165b37
jpabloroa <jpablonano@gmail.com>
2021-12-03 15:47:56 -0500

    Correcci├│n de errores

--- Modelo/Database.php
+++ Modelo/Database.php
@@ -94,15 +94,14 @@ class Database
 
             //if ($params && count($params) >= 1) {
             $stmt->bind_param(
-                "ibssss",
+                "isssss",
                 // Parametros
-                $params[0],
-                $params[1],
-                $params[2],
-                $params[3],
-                $params[4],
-                $params[5],
-                $params[6],
+                $params["fechaDeCreacion"],
+                $params["nombre"],
+                $params["correo"],
+                $params["celular"],
+                $params["medioPublicitario"],
+                $params["proyectoDeInteres"]
             );
             /*} else {
                 throw new Exception("Error de sintáxis, compruebe el tamaño de params ");
