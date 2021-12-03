a99eaf39a6fdf1e230fca8eada12bc51f5f71504
jpabloroa <jpablonano@gmail.com>
2021-12-03 15:32:01 -0500

    Correcci├│n de errores

--- META-DATA/scripts/conexion-bbdd.js
+++ META-DATA/scripts/conexion-bbdd.js
@@ -49,7 +49,7 @@ function nuevaFilaCliente(columna, title, info, valor, option) {
             valorHTML = `<select>${(valor != null) ? "<option selected='selected' value='true'>SI</option><option>NO</option>" : "<option selected='selected' value='false'>NO</option><option>SI</option>"}</select><input contenteditable="false" ${(valor != null) ? "value='" + valor + "'" : ""} style="display:none;" type="text">`;
             break;
         case "fecha":
-            valorHTML = `<input type="date" ${(valor == null) ? "" : "value='" + new Date(valor).toISOString() + "'"}>`;
+            valorHTML = `<input type="date" ${(valor == null) ? "" : "value='" + valor + "'"}>`;
             break;
         default:
             valorHTML = `${(valor == null) ? "-" : valor}`
