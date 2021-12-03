562e60339f3320f8da79c6a6a762cbd210e46950
jpabloroa <jpablonano@gmail.com>
2021-12-03 12:14:56 -0500

    Correcci├│n de errores

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ for ($i = 0; $i < count($inputUri); $i++) {
     }
 }
 
-if (isset($parsedUri[0]) && $parsedUri == "clientes") {
+if (isset($parsedUri[0]) && $parsedUri[0] == "clientes") {
     echo "calmasion";
 }
 
