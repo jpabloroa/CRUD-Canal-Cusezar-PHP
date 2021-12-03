3c48e630ba120c31504b62ef241761df885719a2
jpabloroa <jpablonano@gmail.com>
2021-12-03 11:17:09 -0500

    Correcci├│n de errores

--- index.php
+++ index.php
@@ -3,15 +3,18 @@ require __DIR__ . "/Config/bootstrap.php";
  
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
+
+for($i = 0; $i>count($uri);$i++){
+    echo "-".$i.": ".$uri[$i];
+}
  
-if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+/*if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
     header("HTTP/1.1 404 Not Found");
     exit();
-}
+}*/
  
 require PROJECT_ROOT_PATH . "/Controlador/api/UserController.php";
  
 $objFeedController = new UserController();
 $strMethodName = $uri[3] . 'Action';
 $objFeedController->{$strMethodName}();
-?>
