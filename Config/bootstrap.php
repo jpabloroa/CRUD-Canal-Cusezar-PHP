e898fdf27906504c90a4822a1390e21e6b962883
jpabloroa <jpablonano@gmail.com>
2021-12-03 10:33:11 -0500

    Correcci├│n de errores

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -2,11 +2,11 @@
 define("PROJECT_ROOT_PATH", __DIR__ . "/../");
  
 // include main configuration file
-require_once PROJECT_ROOT_PATH . "/inc/config.php";
+require_once PROJECT_ROOT_PATH . "/Config/config.php";
  
 // include the base controller file
-require_once PROJECT_ROOT_PATH . "/Controller/Api/BaseController.php";
+require_once PROJECT_ROOT_PATH . "/Controlador/api/BaseController.php";
  
 // include the use model file
-require_once PROJECT_ROOT_PATH . "/Model/UserModel.php";
+require_once PROJECT_ROOT_PATH . "/Modelo/UserModel.php";
 ?>
