d9f5278b9b44eecd74bf8d622447ca9ca8541417
jpabloroa <jpablonano@gmail.com>
2021-12-06 18:24:55 -0500

    Correcci├│n de errores

--- META-DATA/scripts/conexion-bbdd.js
+++ META-DATA/scripts/conexion-bbdd.js
@@ -176,21 +176,23 @@ function getClienteFromDiv(element, hasChanged) {
     for (var i = 0; i < array.length; i++) {
         if (array[i].getElementsByTagName("select").length > 0) {
             if (array[i].getAttribute("tipo") == "opcion") {
-                obj[array[i].getAttribute("columna")] = array[i].getElementsByTagName("select")[0].value;
-            } else {
-                if (array[i].getAttribute("tipo") == "opcion-fecha") {
-                    obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == null) ? null : array[i].getElementsByTagName("input")[0].value;
-                } else if (array[i].getAttribute("tipo") == "opcion-texto") {
-                    obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == "") ? null : array[i].getElementsByTagName("input")[0].value;
-                }
+                //obj[array[i].getAttribute("columna")] = array[i].getElementsByTagName("select")[0].value;
+                obj[array[i].getAttribute("columna")] = "tipo:opcion";
+            } else if (array[i].getAttribute("tipo") == "opcion-fecha") {
+                //obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == null) ? null : array[i].getElementsByTagName("input")[0].value;
+                obj[array[i].getAttribute("columna")] = "tipo:opcion-fecha";
+            } else if (array[i].getAttribute("tipo") == "opcion-texto") {
+                //obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == "") ? null : array[i].getElementsByTagName("input")[0].value;
+                obj[array[i].getAttribute("columna")] = "tipo:opcion-texto";
             }
+        } else if (array[i].getAttribute("tipo") == "fecha") {
+            //obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == "") ? null : array[i].getElementsByTagName("input")[0].value;
+            obj[array[i].getAttribute("columna")] = "tipo:fecha";
         } else {
-            if (array[i].getAttribute("tipo") == "fecha") {
-                obj[array[i].getAttribute("columna")] = (array[i].getElementsByTagName("input")[0].value == "") ? null : "perico";
-            } else {
-                obj[array[i].getAttribute("columna")] = array[i].innerHTML;
-            }
+            //obj[array[i].getAttribute("columna")] = array[i].innerHTML;
+            obj[array[i].getAttribute("columna")] = "tipo:que verga";
         }
+
     }
     obj.codigoConteo = element.parentNode.id.split("-")[1];
     console.log(obj);
