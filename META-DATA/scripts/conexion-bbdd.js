aa1c35dc46e93d857ece47828d85f04afcfc90bd
jpabloroa <jpablonano@gmail.com>
2021-12-06 21:29:44 -0500

    Correcci├│n de errores

--- META-DATA/scripts/conexion-bbdd.js
+++ META-DATA/scripts/conexion-bbdd.js
@@ -87,14 +87,13 @@ function cargarClientes_agregarFila(obj) {
 document.addEventListener('contextmenu', function (e) {
     var context_menu = document.getElementById("context-menu-deployable");
     context_menu.style.display = "block";
-    context_menu.style.top = `${e.clientY}px`;
-    context_menu.style.left = `${e.clientX}px`;
+    context_menu.style.top = `${e.clientY - 5}px`;
+    context_menu.style.left = `${e.clientX - 5}px`;
     context_menu.onblur = function () {
         context_menu.style.display = "none";
     };
     var element = document.elementFromPoint(e.screenX, e.clientY);
-    //context_menu.innerHTML = (element.getAttribute("info") == null) ? `No hay información` : element.getAttribute("info").replace("-", element.getAttribute("valor"));
-    context_menu.innerHTML = `Y-> ${e.screenY}px / X-> ${e.screenX}px`;
+    context_menu.innerHTML = (element.getAttribute("info") == null) ? `No hay información` : element.getAttribute("info").replace("-", element.getAttribute("valor"));
     e.preventDefault();
 }, false);
 
